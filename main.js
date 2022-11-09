8603df6a37219cbb08fde9a62de9617dc484b101
Lucas Lang <lucaslang2010@gmail.com>
2022-11-09 13:20:18 -0300

    New Update

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ const d = document
 const rock = d.getElementById('rock')
 const paper = d.getElementById('paper')
 const scissors= d.getElementById('scissors')
+const player_span_score = d.getElementById("youPoints")
+const cpu_span_score = d.getElementById("cpuPoints")
+let playerPoints = 0
+let cpuPoints = 0
 
 
 function computerChoice(){
@@ -22,11 +26,40 @@ function userChoice(){
     })
 }
 
+function PlayerWinPoints(){
+    playerPoints = playerPoints + 1
+    player_span_score.innerHTML = playerPoints
+}
+
+function cpuWinPoints(){
+    cpuPoints = cpuPoints + 1
+    cpu_span_score.innerHTML = cpuPoints
+}
+
 function game(choice){
     const cpuChoice = computerChoice()
     console.log("Player Choice ==> ", choice);
     console.log("CPU Choice ==> ", cpuChoice);
-}
+    switch (choice + cpuChoice){
+        case "rockscissors":
+        case "paperrock":
+        case "scissorspaper":
+            console.log("You Win")
+            PlayerWinPoints()
+            break;
+        case "rockrock":
+        case "paperpaper":
+        case "scissorsscissors":
+            console.log("There was a tie!")
+            break;
+        case "rockpaper":
+        case "paperscissors":
+        case "scissorsrock":
+            console.log("You lose!")
+            cpuWinPoints()
+            break;
+    }
 
+}
 
 userChoice()
