1e0a70ca01b58388726f9a436afc346133aba46e
Lucas Lang <lucaslang2010@gmail.com>
2022-11-14 10:42:00 -0300

    Update

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
+/* PRINCIPAL */
 const d = document
 const rock = d.getElementById('rock')
 const paper = d.getElementById('paper')
 const scissors= d.getElementById('scissors')
-const player_span_score = d.getElementById("youPoints")
-const cpu_span_score = d.getElementById("cpuPoints")
+const player_div_score = d.getElementById("pPoint")
+const cpu_div_score = d.getElementById("cpuPoint")
 let playerPoints = 0
 let cpuPoints = 0
-
+/* EXTRAS */
+const massage = d.querySelector("#massage")
 
 function computerChoice(){
     let computer = ['rock','paper','scissors'];
@@ -16,24 +18,44 @@ function computerChoice(){
 
 function userChoice(){
     rock.addEventListener('click', function(){
+        massage.innerHTML = "You have chosen the power of the rock"
         game('rock');
     })
     paper.addEventListener('click', function(){
+        massage.innerHTML = "You have chosen the power of the paper"
         game('paper');
     })
     scissors.addEventListener('click', function(){
+        massage.innerHTML = "You have chosen the power of scissors"
         game('scissors');
     })
 }
 
 function PlayerWinPoints(){
     playerPoints = playerPoints + 1
-    player_span_score.innerHTML = playerPoints
+    player_div_score.innerHTML = playerPoints
+    if (playerPoints == 5){
+        return setTimeout(gameAlertPlayer,500);
+    }
 }
 
 function cpuWinPoints(){
     cpuPoints = cpuPoints + 1
-    cpu_span_score.innerHTML = cpuPoints
+    cpu_div_score.innerHTML = cpuPoints
+    if (cpuPoints >= 5){
+        return setTimeout(gameAlertCpu,500);
+    }
+}
+
+function gameAlertPlayer(){
+    player_div_score.innerHTML = 0
+    cpu_div_score.innerHTML = 0
+    alert("You win the match!")
+}
+function gameAlertCpu(){
+    player_div_score.innerHTML = 0
+    cpu_div_score.innerHTML = 0
+    alert("You lose the match...")
 }
 
 function game(choice){
@@ -61,5 +83,6 @@ function game(choice){
     }
 
 }
+
 
 userChoice()
